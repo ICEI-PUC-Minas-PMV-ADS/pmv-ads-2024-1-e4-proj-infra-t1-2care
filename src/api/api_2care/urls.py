a01fb0161350cbaf15afcbc89e9eb3350d379737
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2024-03-16 10:12:45 -0300

    Swagger

--- src/api/api_2care/urls.py
+++ src/api/api_2care/urls.py
@@ -34,6 +34,20 @@ from core.views import (
     UserLogin,
     UserLogout,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="2Care API",
+      default_version='v1',
+      description="2Care API documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -61,5 +75,9 @@ urlpatterns = [
     path('register/', UserSignup.as_view(), name='register'),
     path('login/', UserLogin.as_view(), name='login'),
     path('logout/', UserLogout.as_view(), name='logout'),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
