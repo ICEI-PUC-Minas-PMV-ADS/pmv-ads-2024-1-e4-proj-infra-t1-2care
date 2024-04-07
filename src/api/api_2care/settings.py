386d23c852c7689aea8bfe40ce8e7a490f23f133
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2024-04-07 09:57:02 -0300

    [ feat ] Prod config

--- src/api/api_2care/settings.py
+++ src/api/api_2care/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,27 +84,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "api_2care.wsgi.application"
 
+if os.getenv('PRODUCTION', '') == 'True':
+    # Database
+    # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': '2care_database',
-#         'USER': 'user',
-#         'PASSWORD': 'password',
-#         'HOST': 'db',  # Nome do serviço no docker-compose
-#         'PORT': '5432',
-#     }
-# }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': '2care_database',
+            'USER': 'user',
+            'PASSWORD': 'password',
+            'HOST': 'db',  # Nome do serviço no docker-compose
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
