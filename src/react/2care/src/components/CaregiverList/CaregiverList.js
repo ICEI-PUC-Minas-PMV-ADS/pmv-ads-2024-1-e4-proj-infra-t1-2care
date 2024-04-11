01e1f3c1b6ad4e464895eae070614fa4a43a379c
Ellen Gonçalves <ellenctgoncalves@gmail.com>
2024-04-11 01:58:27 -0300

    [feature] Standarize app.css

--- src/react/2care/src/components/CaregiverList/CaregiverList.js
+++ src/react/2care/src/components/CaregiverList/CaregiverList.js
@@ -4,7 +4,6 @@ import './CaregiverList.css'
 const CaregiverList = () => {
     return (
         <div className='caregiverList'>
-            {/* <h1>Veja alguns cuidadores cadastrados: </h1> */}
             <div className='cards'>
                 <CaregiverCard name='Maria Fontes' especialization='Enfermagem' image='https://img.freepik.com/fotos-gratis/enfermeira-negra-em-seu-espaco-de-trabalho_52683-100571.jpg'></CaregiverCard>
             </div>
--- src/react/2care/src/components/Profile/ProfileCard/ProfileCard.js
+++ src/react/2care/src/components/Profile/ProfileCard/ProfileCard.js
@@ -1,16 +1,15 @@
 import NavigationButton from '../../NavigationButton/NavigationButton';
-import './ProfileCard.css'
 import { Link } from 'react-router-dom';
 
 const ProfileCard = () => {
     return (
         <div className='profileCard'>
-            <h2>Ellen Gonçalves</h2>
+            <h2>Maria Fontes</h2>
             <h3>Cuidador</h3>
             {/* 
             Client
             <h6>Familiar</h6> */}
-            <img src='https:\\github.com\ellen-goncalves.png'></img>
+            <img src='https://img.freepik.com/fotos-gratis/enfermeira-negra-em-seu-espaco-de-trabalho_52683-100571.jpg'></img>
             <NavigationButton to="/requests" text="Propostas Recebidas" />
             <button>Agenda</button>
             <NavigationButton to="/request" text="Enviar proposta" />
