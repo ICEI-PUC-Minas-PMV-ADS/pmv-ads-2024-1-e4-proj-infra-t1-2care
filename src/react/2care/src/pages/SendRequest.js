aa04e5d6c6e3f1d18a3a100407726c936bdc3412
Amanda <amandaventura2014@gmail.com>
2024-04-20 09:32:19 -0300

    [feature] Update: SendRequest and App.css

--- src/react/2care/src/pages/SendRequest.js
+++ src/react/2care/src/pages/SendRequest.js
@@ -1,37 +1,3 @@
-// import NavBar from "../components/NavBar/NavBar";
-// import TopBar from "../components/TopBar/TopBar";
-// import { useTheme } from '@mui/material/styles';
-// import './App.css'
-// import RequestCard from "../components/Requests/RequestCard";
-// import { useEffect } from "react";
-
-
-// const SendRequest = () => {
-//     const theme = useTheme();
-//     useEffect(() => {
-//         document.title = 'Envie uma proposta';
-//     }, []);
-
-//     return (
-//         <div>
-//             <TopBar></TopBar>
-//             <NavBar></NavBar>
-//             <header>
-//                 <h1>Envie uma proposta: </h1>
-//             </header>
-//             <main>
-//                 <form>
-//                     <label>Selecione o horário: </label>
-//                     <input type='datetime-local'></input>
-//                 </form>
-//                 <RequestCard></RequestCard>
-//             </main>
-//         </div>
-//     )
-// }
-
-// export default SendRequest;
-
 import React, { useState } from "react";
 import NavBar from "../components/NavBar/NavBar";
 import TopBar from "../components/TopBar/TopBar";
