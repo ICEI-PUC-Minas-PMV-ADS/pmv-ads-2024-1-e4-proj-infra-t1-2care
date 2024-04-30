cd9283b55924459c6cca3ae3244114e5037889ee
Amanda <amandaventura2014@gmail.com>
2024-04-30 16:47:12 -0300

    [feature] Novo RequestsCareReceiver.jsx

---
+++ src/react/2care/src/pages/carereceiver/RequestsCareReceiver.jsx
@@ -0,0 +1,37 @@
+import NavBar from "../../components/NavBar/NavBar";
+import RequestList from "../../components/Requests/RequestList";
+import TopBar from "../../components/TopBar/TopBar";
+import { useTheme } from '@mui/material/styles';
+import { useEffect } from "react";
+
+const RequestsCareReceiver = ({ userType }) => {
+    const theme = useTheme();
+    useEffect(() => {
+        document.title = userType === 'caregiver' ? 'Propostas Recebidas' : 'Propostas Enviadas';
+    }, [userType]);
+
+    const getHeaderText = () => {
+        if (userType === 'caregiver') {
+            return 'Veja as suas propostas recebidas:';
+        } else if (userType === 'carereceiver') {
+            return 'Veja as propostas enviadas por você:';
+        } else {
+            return 'Veja suas propostas:';
+        }
+    };
+
+    return (
+        <div>
+            <TopBar></TopBar>
+            <NavBar></NavBar>
+            <header>
+                <h1>{getHeaderText()}</h1>
+            </header>
+            <main>
+                <RequestList userType={userType}></RequestList>
+            </main>
+        </div>
+    )
+}
+
+export default RequestsCareReceiver;
