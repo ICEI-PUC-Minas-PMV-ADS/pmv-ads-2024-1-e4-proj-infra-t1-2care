6389097b1908737c2d92e6f9abf94d977a440ba9
Amanda <amandaventura2014@gmail.com>
2024-05-04 14:29:33 -0300

    [feature] Update CaregiverCard

--- src/react/2care/src/components/CaregiverCard/CaregiverCard.js
+++ src/react/2care/src/components/CaregiverCard/CaregiverCard.js
@@ -3,30 +3,18 @@ import RatingStars from '../Ratings/RatingStars';
 import './CaregiverCard.css';
 
 const CaregiverCard = (props) => {
-    const [showSpecialization, setShowSpecialization] = useState(false);
-
-    const handleMouseEnter = () => {
-        setShowSpecialization(true);
-    };
-
-    const handleMouseLeave = () => {
-        setShowSpecialization(false);
-    };
-
     return (
-        <div className='caregiverCard' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className='caregiverCard'>
             <img src={props.image} alt={props.name} />
             {props.showName && (
                 <h4>{props.name}</h4>
             )}
-            {(props.showDescription || showSpecialization) && (
-                <div className='description'>
-                    <p>Especialização: {props.especialization}</p>
-                    <p>Distância: {props.distance ? props.distance : 'cadastre-se para descobrir!'}</p>
-                    <p>Avaliações: </p>
-                    <RatingStars stars={3.7} />
-                </div>
-            )}
+            <div className='description'>
+                <p>Especialização: {props.especialization}</p>
+                <p>Distância: {props.distance ? props.distance : 'cadastre-se para descobrir!'}</p>
+                <p>Avaliações: </p>
+                <RatingStars stars={3.7} />
+            </div>
         </div>
     );
 };
