5330efc8f06b67ad44f4bf80aec939dc865d6b47
shinjidotdev <github@shinji.dev>
2024-06-03 18:37:19 -0300

    [feat] Visualização de Avaliações Deslogado

--- src/react-native/2care/components/Evaluation/CardEvaluation.js
+++ src/react-native/2care/components/Evaluation/CardEvaluation.js
@@ -1,58 +1,62 @@
-// import React from 'react';
-// import { View, StyleSheet } from 'react-native';
-// import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-// const CardEvaluation = ({ clienteName }) => {
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.leftContent}>
-//         <View style={styles.clientNameContainer}>
-//           <MaterialIcons name="person" size={20} color="#486142" />
-//           <Text style={styles.clientName}>{clienteName}</Text>
-//         </View>
-//         <View style={styles.starsContainer}>
-//           {[1, 2, 3, 4, 5].map((number) => (
-//             <MaterialIcons
-//               key={number}
-//               name="star"
-//               size={20}
-//               color={number <= 3 ? "#ED8733CC" : "#E0E0E0"}
-//               style={styles.star}
-//             />
-//           ))}
-//         </View>
-//       </View>
-//     </View>
-//   );
-// };
+const CardEvaluation = ({ evaluation }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContent}>
+        <View style={styles.clientNameContainer}>
+          <MaterialIcons name="person" size={20} color="#486142" />
+          <Text style={styles.clientName}>{evaluation.care_receiver.name}</Text>
+        </View>
+        <View style={styles.starsContainer}>
+          {[1, 2, 3, 4, 5].map((number) => (
+            <MaterialIcons
+              key={number}
+              name="star"
+              size={20}
+              color={number <= evaluation.rating ? "#ED8733CC" : "#E0E0E0"}
+              style={styles.star}
+            />
+          ))}
+        </View>
+        <Text style={styles.description}>{evaluation.description}</Text>
+      </View>
+    </View>
+  );
+};
 
-// const styles = StyleSheet.create({
-//   container: {
-//     borderWidth: 1,
-//     borderColor: '#799275',
-//     borderRadius: 10,
-//     padding: 10,
-//     marginBottom: 10,
-//   },
-//   leftContent: {
-//     flexDirection: 'column',
-//   },
-//   clientNameContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     marginBottom: 5,
-//   },
-//   clientName: {
-//     marginLeft: 5,
-//     color: '#486142',
-//   },
-//   starsContainer: {
-//     flexDirection: 'row',
-//     marginBottom: 5,
-//   },
-//   star: {
-//     marginRight: 2,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: '#799275',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  leftContent: {
+    flexDirection: 'column',
+  },
+  clientNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  clientName: {
+    marginLeft: 5,
+    color: '#486142',
+  },
+  starsContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  star: {
+    marginRight: 2,
+  },
+  description: {
+    color: '#486142',
+  },
+});
 
-// export default CardEvaluation;
+export default CardEvaluation;
--- src/react-native/2care/services/apiServiceMob.js
+++ src/react-native/2care/services/apiServiceMob.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://127.0.0.1:8000/api";
+export const API_URL = "https://api.2care.site/api";
 //const API_URL = process.env.NODE_ENV === 'development' ? REACT_APP_DEV_MODE : REACT_APP_PROD_MODE;
