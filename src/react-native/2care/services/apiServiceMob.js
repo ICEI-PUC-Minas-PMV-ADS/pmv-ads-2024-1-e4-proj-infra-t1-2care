c8357b65f34de31787afff490d2bf8523389f20d
shinjidotdev <github@shinji.dev>
2024-06-15 20:03:09 -0300

    [feat] Dockerfile e Docker-compose

--- src/react-native/2care/services/apiServiceMob.js
+++ src/react-native/2care/services/apiServiceMob.js
@@ -1,3 +1,3 @@
-//export const API_URL = "https://api.2care.site/api"; // Descomentar depois do deploy
-export const API_URL = "http://127.0.0.1:8000/api"; // Comentar depois do deploy
+export const API_URL = "https://api.2care.site/api"; // Descomentar depois do deploy
+//export const API_URL = "http://127.0.0.1:8137/api"; // Comentar depois do deploy
 //const API_URL = process.env.NODE_ENV === 'development' ? REACT_APP_DEV_MODE : REACT_APP_PROD_MODE;
